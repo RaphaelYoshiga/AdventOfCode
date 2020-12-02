4d5cc24c8665b87bd7bd8a4d5cde3086f78fba9b
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-12-02 22:42:10 +0000

    initial commit

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var readAllLines = File.ReadAllLines("input.txt");
+            var nums = readAllLines.Select(int.Parse).ToList();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                var num = nums[i];
+                for (int y = i; y < nums.Count; y++)
+                {
+                    if (y == i)
+                        continue;
+                    
+                    var otherNum = nums[y];
+
+
+                    for (int j = 0; j < nums.Count; j++)
+                    {
+                        if (j == y || j == i)
+                            continue;
+
+                        var anotherNum = nums[j];
+
+                        if (otherNum + num + anotherNum == 2020)
+                        {
+                            Console.WriteLine($"{num} + {otherNum} + {anotherNum} = {num * otherNum * anotherNum}");
+                        }
+
+                    }
+
+
+
+
+                }
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
